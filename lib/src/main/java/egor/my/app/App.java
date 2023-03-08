b1052ad3818b25a4d5b7681275bceef724cde221
andrey <andrey@example.com>
2023-03-08 00:31:43 -0500

    converted into multi modules project.

---
+++ lib/src/main/java/egor/my/app/App.java
@@ -0,0 +1,13 @@
+package egor.my.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
