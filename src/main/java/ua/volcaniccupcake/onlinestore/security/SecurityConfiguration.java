f6cc20215a1057b27f2c3a3efcbe3fca92124bfc
Egor Serdyuk <volcanic-cupcake@proton.me>
2023-04-19 17:11:56 +0300

    Basic http authentication

--- src/main/java/ua/volcaniccupcake/onlinestore/security/SecurityConfiguration.java
+++ src/main/java/ua/volcaniccupcake/onlinestore/security/SecurityConfiguration.java
@@ -2,21 +2,22 @@ package ua.volcaniccupcake.onlinestore.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
 
 @Configuration
 public class SecurityConfiguration {
 
-   /*@Bean
+   @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-       http
-           .authorizeHttpRequests()
-           .requestMatchers("/login").permitAll()
-           .anyRequest().authenticated();*/
-       /*http
-           .formLogin(form -> form
-                .loginPage("/login")
-                .permitAll()
-            );*/
-    //return http.build();
-    //}
+       http.authorizeHttpRequests()
+               .requestMatchers("/brewery").permitAll()
+               .anyRequest().authenticated()
+               .and()
+               .httpBasic()
+               .and()
+               .csrf().disable();
+        return http.build();
+    }
 }
--- src/test/java/ua/volcaniccupcake/onlinestore/controller/ProductControllerTest.java
+++ src/test/java/ua/volcaniccupcake/onlinestore/controller/ProductControllerTest.java
@@ -1,19 +1,30 @@
 package ua.volcaniccupcake.onlinestore.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.mock.mockito.SpyBean;
 import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 import ua.volcaniccupcake.onlinestore.model.Product;
 import ua.volcaniccupcake.onlinestore.repository.CountryRepository;
 import ua.volcaniccupcake.onlinestore.repository.ProductRepository;
 import ua.volcaniccupcake.onlinestore.service.ProductService;
 
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.httpBasic;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -22,6 +33,9 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @AutoConfigureMockMvc
 @SpringBootTest
 class ProductControllerTest {
+
+    @Autowired
+    WebApplicationContext wac;
 
     @Autowired
     private MockMvc mvc;
@@ -38,18 +52,28 @@ class ProductControllerTest {
     @Autowired
     private ObjectMapper objectMapper;
 
+    @BeforeEach
+    void setUp() {
+        mvc = MockMvcBuilders
+                .webAppContextSetup(wac)
+                .apply(springSecurity())
+                .build();
+    }
+
 
     @Test
     void getAllProduct() throws Exception {
-        mvc.perform(get("/product"))
+        mvc.perform(get("/product").with(httpBasic("user", "password")))
                 .andExpect(status().isOk());
     }
 
     @Test
+    @Disabled
     void getProductById() {
     }
 
     @Test
+    @Disabled
     void createProduct() throws Exception {
         Product product = new Product();
         product.setName("monitor");
@@ -63,14 +87,17 @@ class ProductControllerTest {
     }
 
     @Test
+    @Disabled
     void updateCourse() {
     }
 
     @Test
+    @Disabled
     void deleteProductById() {
     }
 
     @Test
+    @Disabled
     void deleteProduct() {
     }
 }
