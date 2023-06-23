7442537720982f994db64ea81900789b7f5d1eb0
Egor Serdyuk <volcanic-cupcake@proton.me>
2023-06-23 15:56:16 +0300

    document REST api

--- src/main/java/ua/volcaniccupcake/onlinestore/controller/OrderController.java
+++ src/main/java/ua/volcaniccupcake/onlinestore/controller/OrderController.java
@@ -1,5 +1,7 @@
 package ua.volcaniccupcake.onlinestore.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -14,18 +16,21 @@ import java.util.Set;
 @RestController
 @RequestMapping("/api/order")
 @RequiredArgsConstructor
+@Tag(name = "Order Controller", description = "This REST controller provides services to manage orders in the Online Store Application")
 public class OrderController {
     private final OrderService orderService;
 
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
+    @Operation(summary = "Creates a new order for a specific user")
     public void createOrder(@RequestBody OrderDTO orderDTO,
                             @AuthenticationPrincipal User user) {
         orderService.save(user.getCustomer(), orderDTO);
     }
 
     @GetMapping
+    @Operation(summary = "Provides orders for a specific user")
     public Set<OrderDTO> getOrdersByCustomerId(@AuthenticationPrincipal User user) {
         return orderService.listOrdersByCustomerId(user.getCustomer().getId());
     }
--- src/main/java/ua/volcaniccupcake/onlinestore/controller/UserController.java
+++ src/main/java/ua/volcaniccupcake/onlinestore/controller/UserController.java
@@ -1,5 +1,7 @@
 package ua.volcaniccupcake.onlinestore.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -10,11 +12,13 @@ import ua.volcaniccupcake.onlinestore.service.UserService;
 @RestController
 @RequestMapping("/api/user")
 @RequiredArgsConstructor
+@Tag(name = "User Controller", description = "This REST controller provides services to manage product in the Online Store Application")
 public class UserController {
     private final UserService userService;
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
+    @Operation(summary = "Registers a new user")
     public void registerNewUser(@RequestBody UserDTO userDTO) {
         userService.registerNewUser(userDTO);
     }
