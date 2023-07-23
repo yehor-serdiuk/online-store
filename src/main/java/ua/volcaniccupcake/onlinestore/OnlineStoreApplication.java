57678b43907f15d71c06db73c6ec4fe569aec4f9
Egor Serdyuk <volcanic-cupcake@proton.me>
2023-07-23 09:07:59 +0300

    general config rework

--- src/main/java/ua/volcaniccupcake/onlinestore/OnlineStoreApplication.java
+++ src/main/java/ua/volcaniccupcake/onlinestore/OnlineStoreApplication.java
@@ -3,10 +3,12 @@ package ua.volcaniccupcake.onlinestore;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import ua.volcaniccupcake.onlinestore.config.AppProperties;
 
 @SpringBootApplication
 @EnableConfigurationProperties(AppProperties.class)
+@EnableMethodSecurity
 public class OnlineStoreApplication {
 
 	public static void main(String[] args) {
